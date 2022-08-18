d43d214083989f6b45f51ad86ce04dcdb970f6c4
nezaga <nicolas.zaga@gmail.com>
2022-08-17 22:29:02 -0300

    primer commit de app de react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import './estilos.css';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  let imagen = "kqXt7Sq2.png";
+  let titulo = "Doble Cuarto de Libra con Queso";
+  let calorias = "771 kcal";
+  let descripcion = "Imaginá la sensación del clásico Cuarto de Libra. Imaginalo con un medallón de deliciosa carne 100% vacuna, queso cheddar, cebolla, kétchup y mostaza ¿Listo? Ahora duplicá esa sensación. Ya tenés el Doble Cuarto en la cabeza.";
+  const producto = {id:1, image:imagen, title:titulo, calories:calorias, description:descripcion};
+
   return (
-    <div className="App">
+    <div className="container">
+        <div className="row">
+          <div className="col-md-4 offset-md-2">
+            <img src={producto.image} alt={titulo} className="img-fluid"/>
+          </div>
+          <div className="col-md-4">
+            <h1>{producto.title}</h1>
+            <p>{calorias}</p>
+            <p>{producto.description}</p>
+          </div>
+        </div>
+      </div>
+     
+    
+       
+      
+      /*{ <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,7 +42,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </div>} */
   );
 }
 
