64f1cfec6ad72ec2c7c2f2cd2d0d706e99e11f4a
nezaga <nicolas.zaga@gmail.com>
2022-08-28 09:28:54 -0300

    Desafío Menu E-Commerce

--- src/App.js
+++ src/App.js
@@ -1,39 +1,14 @@
 import './estilos.css';
-//import logo from './logo.svg';
-//import './App.css';
 import 'core-js/actual';
+import NavBar from './components/NavBar';
 
 
 function App() {
-  console.log(Array.from(new Set([1,2,3,4,1])));
-  
-  console.log([1,[2,3],[4,[5,[6,7]]]]);
-  console.log([1,[2,3],[4,[5,[6,7]]]].flat(2));
-
   return (
     <div>
-
+      <NavBar/>
     </div>
     
-    
-    
-      
-      /*{ <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>} */
   );
 }
 
