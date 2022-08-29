19a682748d4378ec82009201a4b6e78e0f975bc7
nezaga <nicolas.zaga@gmail.com>
2022-08-29 19:50:47 -0300

    Desafío Landing

---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ItemListContainer = () => {
+    return (
+        <div>
+            <h1>Titulo</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis architecto, magnam reprehenderit iste voluptate temporibus voluptatum nam! Magni error debitis itaque necessitatibus exercitationem, quibusdam rem nostrum soluta laudantium. Eligendi, repudiandae?</p>
+        </div>
+        
+
+    )
+}
+
+export default ItemListContainer;
