04c6d4e052e855a999b3f17fad0e49e37a66307c
nezaga <nicolas.zaga@gmail.com>
2022-08-29 22:51:26 -0300

    clase 5 - Componentes 2

--- src/App.js
+++ src/App.js
@@ -1,39 +1,81 @@
 import './estilos.css';
-//import logo from './logo.svg';
-//import './App.css';
 import 'core-js/actual';
+import { Component, useState } from 'react';
 
+function SuperForm (props) {
+  console.log(props);
+  return (
+      <div>
+        <h1>{props.title}</h1>
+        {props.children}
+      </div>
+  )
+}
+
+const SuperButton = () => {
+  return (
+    <input type="button" value="Click Me!" />
+  )
+}
+
+class SuperState extends Component {
+  constructor() {
+    super(); // Es obligatorio ejecutar la función super() antes de usar el this en el consturctor
+    this.state = {
+      curso: "React",
+      lugar: "En casita"
+    }
+  }
+
+  updateName = () => {
+    this.setState(
+      {curso: "ReacTionario"}
+      );
+  }
+
+  render() {
+    return (
+      <div onClick={this.updateName}>
+        <h1>Welcome to Curso de {this.state.curso}. {this.state.lugar}</h1>
+      </div>
+    )
+  }
+}
+
+function StateHook () {
+  const [name, setName] = useState('ReactClass');
+  return (
+    <div onClick={()=> setName('ReactFunction')}>
+      <h2>Hola {name}</h2>
+    </div>
+  )
+}
+
+function ClickCounter () {
+  const [clicks, setClicks] = useState(0);
+  const [fecha, setFecha] = useState(new Date());
+  
+  return (
+    <div onClick={
+      () => {
+        setClicks(clicks +1)
+        setFecha(new Date())
+      }
+    }>
+      <h1>Contador de clicks</h1>
+      <p>Clicks: {clicks} - Fecha: {fecha.getDate()}/{fecha.getMonth()+1}/{fecha.getFullYear()} {fecha.getHours()}:{fecha.getMinutes()}:{fecha.getSeconds()}</p>
+    </div>
+  );
+}
 
 function App() {
-  console.log(Array.from(new Set([1,2,3,4,1])));
-  
-  console.log([1,[2,3],[4,[5,[6,7]]]]);
-  console.log([1,[2,3],[4,[5,[6,7]]]].flat(2));
-
   return (
-    <div>
-
-    </div>
-    
-    
-    
-      
-      /*{ <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>} */
+      <SuperForm title='Hey!'>
+        <SuperButton/>
+        <SuperState/>
+        <StateHook/>
+        <ClickCounter/>
+      </SuperForm>
   );
 }
 
